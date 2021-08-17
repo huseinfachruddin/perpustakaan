883f4520cad17b238c8921da015db50517dab7f6
unknown <husein.18@students.amikom.ac.id>
2021-08-17 22:33:46 +0700

    make it better

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>FISIP</title>
+  </head>
+  <style>
+      body{
+  background: url('bg.jpg') no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  font-family: "Georgia", "Courier New", monospace;
+
+      }
+  </style>
+  <body style="">
+    <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: rgba(0, 59, 253, 0.452);">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Perpustakaan FISIP</a>
+          <div class="spinner-grow text-warning" role="status">
+        </div>
+        </div>
+        
+      </nav>
+<div class="container">
+    <div class="row p-3">
+        <h1 class="text-center">Halo, Politik Muda</h1>
+    </div>
+    <div class="row p-3">
+        <div class="card border-success mb-3 w-100" style=" background-color: rgba(253, 253, 253, 0.603);">
+            <div class="card-body text-dark">
+              <h5 class="card-title">Cari Pengetahuanmu Disini</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer bg-transparent border-success d-flex justify-content-center">
+                
+                <a href="#" class="btn btn-outline-dark fa fa-facebook m-2"></a>
+                <a href="#" class="btn btn-outline-dark fa fa-twitter m-2"></a>
+                <a href="#" class="btn btn-outline-dark fa fa-youtube m-2"></a>
+                <a href="#" class="btn btn-outline-dark fa fa-whatsapp m-2"></a>
+            </div>
+          </div>
+    </div>
+    <div class="row p-3">
+        <div class="input-group justify-content-center ">
+            <div class="form-outline">
+              <input type="search" id="form1" class="form-control" />
+            </div>
+            <a type="button" class="btn btn-primary m-0">
+              <i class="fa fa-search"></i>
+            </a>
+          </div>
+    </div>
+    <div class="row">
+        <a href="https://drive.google.com/drive/folders/1E-Kvvl0Gad5iktL7OSUEzvPJKaw3hOMN" class="col-6 p-2 justify-content-center">
+                <button type="button" class="btn btn-outline-dark w-100" style="background-color: rgba(253, 228, 0, 0.452);">
+                <strong> Politik</strong>
+                </button>
+        </a>
+        <a href="https://drive.google.com/drive/folders/1E-Kvvl0Gad5iktL7OSUEzvPJKaw3hOMN" class="col-6 p-2 justify-content-center">
+            <button href="https://drive.google.com/drive/folders/1E-Kvvl0Gad5iktL7OSUEzvPJKaw3hOMN" type="button" class="btn btn-outline-dark w-100" style="background-color: rgba(253, 228, 0, 0.452);">
+            <strong> Filsafat</strong>
+            </button>
+        </a>
+    </div>
+    <div class="row">
+        <a href="https://drive.google.com/drive/folders/1E-Kvvl0Gad5iktL7OSUEzvPJKaw3hOMN" class="col-6 p-2 justify-content-center">
+                <button type="button" class="btn btn-outline-dark w-100" style="background-color: rgba(253, 228, 0, 0.452);">
+                <strong> Budaya</strong>
+                </button>
+        </a>
+        <a href="https://drive.google.com/drive/folders/1E-Kvvl0Gad5iktL7OSUEzvPJKaw3hOMN" class="col-6 p-2 justify-content-center">
+            <button type="button" class="btn btn-outline-dark w-100" style="background-color: rgba(253, 228, 0, 0.452);">
+            <strong> Sejarah</strong>
+            </button>
+        </a>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
