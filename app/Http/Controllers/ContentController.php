bb12270c830c259366fd0fb5cc40e969db58e1d5
unknown <husein.18@students.amikom.ac.id>
2021-08-22 01:56:20 +0700

    make it better

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Content;
+
+class ContentController extends Controller
+{
+    public function getContent(){
+        $data = Content::orderBy('id','desc')->get();
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+
+    public function detailContent(Request $request){
+        $data = Content::find($request->id);
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+
+    public function getSearchContent(Request $request){
+        $request->validate([
+            'search' =>'nullable',
+        ]);
+
+        $data = Content::where('name','like',"%".$request->search."%")
+        ->orWhere('desc','like',"%".$request->search."%")->get();
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+
+    public function createContent(Request $request){
+        $request->validate([
+            'name' =>'required',
+            'desc'  =>'nullable',
+            'link'  =>'required',
+        ]);
+
+        $data = new Content;
+        $data->name = $request->name;
+        $data->desc = $request->desc;
+        $data->link = $request->link;
+        $data->save();
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+
+    public function editContent(Request $request){
+        $request->validate([
+            'name' =>'required',
+            'desc'  =>'nullable',
+            'link'  =>'required',
+        ]);
+
+        $data = Content::find($request->id);
+        $data->name = $request->name;
+        $data->desc = $request->desc;
+        $data->link = $request->link;
+        $data->save();
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+
+    public function deleteContent(Request $request){
+
+        $data = Content::find($request->id);
+        $data->delete();
+
+        $response = [
+            'success'=>true,
+            'content'=>$data,
+        ];
+        
+        return response($response,200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/content', [ContentController::class,'getContent']);
+Route::get('/content/{id}', [ContentController::class,'detailContent']);
+Route::post('/content/search', [ContentController::class,'getSearchContent']);
+Route::post('/content/create', [ContentController::class,'createContent']);
+Route::put('/content/edit/{id}', [ContentController::class,'editContent']);
+Route::delete('/content/delete/{id}', [ContentController::class,'deleteContent']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
